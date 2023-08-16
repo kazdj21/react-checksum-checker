b0a44b3656961cb1f4e36821e428c087e3924a88
Kaz <kazdj21@hotmail.com>
2023-08-16 10:17:42 +1000

    Updated manifest.json and added responsiveness to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
+import { Grid } from "@mui/material";
 
 function Header({ children, containerStyle, textStyle }) {
 
     return (
         <>
-            <div style={{...containerStyle}}>
-                <h1 style={{borderBottom: "1px solid white", color: "white", display: "inline-block",  ...textStyle}}>{children}</h1>
-            </div>
+            <Grid container>
+                <Grid item sm={6} md={12} lg={12}>
+                    <div style={{...containerStyle}}>
+                            <h1 style={{borderBottom: "1px solid white", color: "white", display: "inline-block",  ...textStyle}}>{children}</h1>
+                    </div>
+                </Grid>
+            </Grid>
         </>
     );
 
